6089033a3cfc2810afa3a9a97eb5c9807570ff16
lex <luohai2233@163.com>
2024-07-14 22:37:29 +0800

    build static html

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  distDir: "build",
+  output: 'export',
+  images:{
+    unoptimized: true
+  }
+
 };
 
 const withMDX = createMDX({
